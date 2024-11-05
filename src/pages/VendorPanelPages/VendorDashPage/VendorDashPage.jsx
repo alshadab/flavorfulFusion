5070fca1dd3bae8b14a42647199988faa102ffbb
mahisurrahman <zordanblog009@gmail.com>
2024-11-06 03:37:54 +0600

    Completed the Vendor DASHBOARD

--- src/pages/VendorPanelPages/VendorDashPage/VendorDashPage.jsx
+++ src/pages/VendorPanelPages/VendorDashPage/VendorDashPage.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import SummaryDataCards from "../../../components/GlobalComponents/SummaryDataCards/SummaryDataCards";
+import { Link } from "react-router-dom";
 
-import { FcMoneyTransfer } from "react-icons/fc";
+// import { FcMoneyTransfer } from "react-icons/fc";
 import { FcCurrencyExchange } from "react-icons/fc";
 import { FcFactoryBreakdown } from "react-icons/fc";
 import { FcBusinessman } from "react-icons/fc";
@@ -17,14 +18,85 @@ function VendorDashPage() {
 
   const [postRequest, getRequest] = useRequest();
   const [totalRevenues, setTotalRevenues] = useState(0);
+  const [totalProducts, setTotalProducts] = useState([]);
+  const [totalOrders, setTotalOrders] = useState([]);
+  const [totalPendingOrders, setTotalPendingOrders] = useState([]);
+  const [totalConfirmedOrders, setTotalConfirmedOrders] = useState([]);
+  const [totalDeliveredOrders, setTotalDeliveredOrders] = useState([]);
+  const [totalCancelledOrders, setTotalCancelledOrders] = useState([]);
 
   const fetchVendorRevenues = async () => {
-    const fetchData = await getRequest("/products/src/rev/byusr", user?._id);
-    setTotalRevenues(fetchData?.data?.data);
+    const fetchData = await postRequest("/products/src/rev/byusr", {
+      userId: user?._id,
+    });
+    setTotalRevenues(fetchData?.data?.data?.totalRevenue);
   };
+
+  const fetchTotalProducts = async () => {
+    try {
+      const data = await postRequest("/products/src/all/byusrid", {
+        userId: user?._id,
+      });
+
+      setTotalProducts(data?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchPendingOrders = async () => {
+    try {
+      const fetechedData = await postRequest("/orders/src/pending/all/byusr", {
+        userId: user?._id,
+      });
+      setTotalPendingOrders(fetechedData?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchCancelOrders = async () => {
+    try {
+      const fetechedData = await postRequest("/orders/src/cancel/all/byusr", {
+        userId: user?._id,
+      });
+      setTotalCancelledOrders(fetechedData?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchConfirmOrders = async () => {
+    try {
+      const fetechedData = await postRequest("/orders/src/confirmed/all/byusr", {
+        userId: user?._id,
+      });
+      setTotalConfirmedOrders(fetechedData?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchDeliveredOrders = async () => {
+    try {
+      const fetechedData = await postRequest("/orders/src/confirmed/all/byusr", {
+        userId: user?._id,
+      });
+      setTotalDeliveredOrders(fetechedData?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(totalDeliveredOrders, "Total CA Orders");
 
   useEffect(() => {
     fetchVendorRevenues();
+    fetchTotalProducts();
+    fetchPendingOrders();
+    fetchCancelOrders();
+    fetchConfirmOrders();
+    fetchDeliveredOrders();
   }, []);
 
   return (
@@ -32,48 +104,44 @@ function VendorDashPage() {
       <h1 className="pl-5 border-l-4 border-orange-600 font-extrabold">
         Summary
       </h1>
-      <div className="mt-5 w-full grid grid-cols-4 gap-x-4">
-        {/* <SummaryDataCards
+      <div className="mt-5 w-full grid grid-cols-3 gap-x-4">
+        <SummaryDataCards
           title="Total Revenue"
-          amount={totalRevenue}
+          amount={totalRevenues}
           borderLColor="border-green-400"
           borderBColor="border-b-green-400"
           isAmount={true}
           icons={FcCurrencyExchange}
-        /> */}
-        {/* <SummaryDataCards
-          title="Total Order"
-          amount={totalOrderNumber}
-          borderLColor="border-red-400"
-          borderBColor="border-b-red-400"
-          isAmount={false}
-          icons={FcMoneyTransfer}
         />
+        <Link to="/vendorproducts">
+          <div className="hover:scale-105 hover:cursor-pointer hover:shadow-lg">
+            <SummaryDataCards
+              title="Total Products"
+              amount={totalProducts.length}
+              borderLColor="border-blue-400"
+              borderBColor="border-b-blue-400"
+              isAmount={false}
+              icons={FcFactoryBreakdown}
+            />
+          </div>
+        </Link>
         <SummaryDataCards
-          title="Vendor"
-          amount={totalVendorNumber.length}
-          borderLColor="border-blue-400"
-          borderBColor="border-b-blue-400"
-          isAmount={false}
-          icons={FcFactoryBreakdown}
-        />
-        <SummaryDataCards
-          title="Total Users"
-          amount={totalUserNumber}
+          title="Total Orders"
+          amount={totalOrders ? totalOrders.length : 0}
           borderLColor="border-orange-400"
           borderBColor="border-b-orange-400"
-          isAmount={false} 
+          isAmount={false}
           icons={FcBusinessman}
-        /> */}
+        />
       </div>
 
       <h1 className="mt-20 pl-5 border-l-4 border-orange-600 font-extrabold">
         Order Status
       </h1>
-      {/* <div className="mt-5 w-full grid grid-cols-4 gap-x-4">
+      <div className="mt-5 w-full grid grid-cols-4 gap-x-4">
         <SummaryDataCards
           title="Pending Orders"
-          amount={totalPendingOrdersNumber}
+          amount={totalPendingOrders ? totalPendingOrders.length : 0}
           borderLColor="border-purple-400"
           borderBColor="border-b-purple-400"
           isAmount={false}
@@ -81,7 +149,7 @@ function VendorDashPage() {
         />
         <SummaryDataCards
           title="Confirmed Orders"
-          amount={totalConfirmedOrdersNumber}
+          amount={totalConfirmedOrders ? totalConfirmedOrders.length : 0}
           borderLColor="border-orange-400"
           borderBColor="border-b-orange-400"
           isAmount={false}
@@ -89,7 +157,7 @@ function VendorDashPage() {
         />
         <SummaryDataCards
           title="Delivered Orders"
-          amount={totalDeliveredOrdersNumber}
+          amount={totalDeliveredOrders ? totalDeliveredOrders.length : 0}
           borderLColor="border-sky-400"
           borderBColor="border-b-sky-400"
           isAmount={false}
@@ -97,13 +165,13 @@ function VendorDashPage() {
         />
         <SummaryDataCards
           title="Cancelled Orders"
-          amount={totalCancelledOrdersNumber}
+          amount={totalCancelledOrders ? totalCancelledOrders.length : 0}
           borderLColor="border-red-400"
           borderBColor="border-b-red-400"
           isAmount={false}
           icons={FcCancel}
         />
-      </div> */}
+      </div>
 
       {/* <div className="mt-20 pl-5 border-l-4 border-orange-600 grid grid-cols-5 items-center justify-between">
         <h1 className="font-extrabold col-span-3">Recent Orders</h1>
