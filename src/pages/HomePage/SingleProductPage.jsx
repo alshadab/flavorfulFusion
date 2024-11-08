c0741421e9e696746776ef08895d1693499f38c5
mahisurrahman <zordanblog009@gmail.com>
2024-11-08 11:37:35 +0600

    Pulling from arif-mahi-pr

--- src/pages/HomePage/SingleProductPage.jsx
+++ src/pages/HomePage/SingleProductPage.jsx
@@ -97,7 +97,7 @@ function SingleProductPage() {
     <div className="container mx-auto px-4 py-8">
       <div className="flex flex-col md:flex-row gap-8 bg-white p-8">
         {/* Product Image and Thumbnails */}
-        <div className="md:w-1/2 flex flex-col items-center">
+        <div className="md:w-1/2 flex flex-col items-center justify-center">
           <div className="relative">
             {/* {productInfo.discount && (
               <span className="absolute top-2 left-2 bg-yellow-400 text-white font-semibold py-1 px-2 rounded-lg text-sm">
@@ -177,6 +177,13 @@ function SingleProductPage() {
               +
             </button>
           </div>
+
+          <div className="mt-3 mb-5 text-lg flex items-center gap-x-2 font-semibold">
+            <span>Total Cost: </span>
+            <span className="text-green-700">{quantity*productInfo?.sellingPrice} TK</span>
+          </div>
+
+
           {quantity === 0 ? (
             <button
               className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-lg mb-6 transition duration-300 disabled:bg-gray-300 disabled:text-gray-500 disabled:cursor-not-allowed"
